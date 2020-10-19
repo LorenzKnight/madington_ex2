c720ef8a31dfd6fcf49a967509632360b588c276
Lorenzo Knight <joellorenzo.k@gmail.com>
2020-10-19 17:01:42 +0200

    Exemple 2

---
+++ src/script.js
@@ -0,0 +1,60 @@
+// 320x250
+function allon1() {
+    var blueoff ='assets/symfonisk-blue-light.png';
+    var redoff ='assets/symfonisk-red-light.png';
+    document.getElementById('foto').setAttribute("style", `background-image: url(${blueoff});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto2').setAttribute("style", `background-image: url(${redoff});-webkit-transition: all .8s ease-in-out`);
+}
+function change1_1() {
+    var blueon ='assets/symfonisk-blue-light.png';
+    var redoff ='assets/symfonisk-red.png';
+    document.getElementById('foto').setAttribute("style", `background-image: url(${blueon});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto2').setAttribute("style", `background-image: url(${redoff});-webkit-transition: all .8s ease-in-out`);
+    // document.getElementById('bgc1').setAttribute("style", `background-image:linear-gradient(to right, white, white, orange); -webkit-transition: all 3.2s ease-in-out`);
+}
+function change2_1() {
+    var redon ='assets/symfonisk-red-light.png';
+    var blueoff ='assets/symfonisk-blue.png';
+    document.getElementById('foto2').setAttribute("style", `background-image: url(${redon});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto').setAttribute("style", `background-image: url(${blueoff});-webkit-transition: all .8s ease-in-out`);
+}
+
+// 800x400
+function allon2() {
+    var blueoff ='assets/symfonisk-blue-light.png';
+    var redoff ='assets/symfonisk-red-light.png';
+    document.getElementById('foto2-1').setAttribute("style", `background-image: url(${blueoff});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto2-2').setAttribute("style", `background-image: url(${redoff});-webkit-transition: all .8s ease-in-out`);
+}
+function change1_2() {
+    var blueon ='assets/symfonisk-blue-light.png';
+    var redoff ='assets/symfonisk-red.png';
+    document.getElementById('foto2-1').setAttribute("style", `background-image: url(${blueon});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto2-2').setAttribute("style", `background-image: url(${redoff});-webkit-transition: all .8s ease-in-out`);
+}
+function change2_2() {
+    var redon ='assets/symfonisk-red-light.png';
+    var blueoff ='assets/symfonisk-blue.png';
+    document.getElementById('foto2-2').setAttribute("style", `background-image: url(${redon});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto2-1').setAttribute("style", `background-image: url(${blueoff});-webkit-transition: all .8s ease-in-out`);
+}
+
+// 300x600
+function allon3() {
+    var blueoff ='assets/symfonisk-blue-light.png';
+    var redoff ='assets/symfonisk-red-light.png';
+    document.getElementById('foto3-1').setAttribute("style", `background-image: url(${blueoff});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto3-2').setAttribute("style", `background-image: url(${redoff});-webkit-transition: all .8s ease-in-out`);
+}
+function change1_3() {
+    var blueon ='assets/symfonisk-blue-light.png';
+    var redoff ='assets/symfonisk-red.png';
+    document.getElementById('foto3-1').setAttribute("style", `background-image: url(${blueon});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto3-2').setAttribute("style", `background-image: url(${redoff});-webkit-transition: all .8s ease-in-out`);
+}
+function change2_3() {
+    var redon ='assets/symfonisk-red-light.png';
+    var blueoff ='assets/symfonisk-blue.png';
+    document.getElementById('foto3-2').setAttribute("style", `background-image: url(${redon});-webkit-transition: all .8s ease-in-out`);
+    document.getElementById('foto3-1').setAttribute("style", `background-image: url(${blueoff});-webkit-transition: all .8s ease-in-out`);
+}
